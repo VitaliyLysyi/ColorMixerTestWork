28825f3e59c527eda8cb6fe8a24bf4b93ac32cf7
VitaliyLysyi <85xq596ijavc7da>
2022-04-23 19:32:42 +0300

    First Day Changes

---
+++ Assets/_Project/Scripts/Ingredient.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Color _color;
+
+    public void JumpToPosition(Vector3 position)
+    {
+        _rigidbody.isKinematic = true;
+
+        DOTween.Sequence()
+            .Append(transform.DOJump(position, 1f, 1, 1f).SetEase(Ease.InSine))
+            .Join(transform.DORotate(Vector3.up * 180, 1f))
+            .AppendCallback(() => _rigidbody.isKinematic = false);
+    }
+
+    public Color GetColor() => _color;
+}
---
+++ Assets/_Project/Scripts/Plate.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Plate : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Ingredient _foodPrefab;
+    public Action _onPlateClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _onPlateClick?.Invoke();
+    }
+
+    public Vector3 GetPosition() => transform.position;
+
+    public Ingredient GetFoodPrefab() => _foodPrefab;
+}
